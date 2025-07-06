94e4dd7c248caff0721545011f2346532b2560a2
Ansh-pal <pansh2004@gmail.com>
2025-07-06 12:06:49 +0530

    first commit

---
+++ src/components/About.js
@@ -0,0 +1,58 @@
+import React from 'react'
+
+export default function About(props) {
+
+let myStyle={
+    color: props.mode==='dark'? 'white':'black',
+    backgroundColor: props.mode==='dark'?'grey':'white',
+    border: '2px solid',
+    borderColor:props.mode==='dark'? 'white':'black'
+}
+
+
+
+  return (
+    <div className='container' style={{color: props.mode==='dark'?'white':'black'}}>
+        <h1 className='my-2'>About Us</h1>
+      <div className="accordion" id="accordionExample" style={myStyle}>
+        <div className="accordion-item" >
+            <h2 className="accordion-header">
+            <button className="accordion-button"  style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+               <strong>Analyze your text</strong> 
+            </button>
+            </h2>
+            <div id="collapseOne" className="accordion-collapse collapse show" style={myStyle} data-bs-parent="#accordionExample">
+            <div className="accordion-body">
+            <strong>TextUtils offers instant text analysis to help you understand and refine your writing. Whether you're checking word count, character count, or estimated reading time, this tool provides accurate metrics within seconds. It's perfect for bloggers, students, and professionals who want quick insights to improve clarity and impact.</strong>
+            </div>
+            </div>
+        </div>
+        <div className="accordion-item">
+            <h2 className="accordion-header">
+            <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                <strong>Free to use</strong>
+            </button>
+            </h2>
+            <div id="collapseTwo" className="accordion-collapse collapse" style={myStyle} data-bs-parent="#accordionExample">
+            <div className="accordion-body">
+                <strong>TextUtils is completely free and doesn't require any sign-up or subscription. You can access all features without limitations—no hidden fees or trials. It's a reliable, open-access utility created to make text editing and analysis accessible for everyone, anytime, anywhere.</strong> 
+            </div>
+            </div>
+        </div>
+        <div className="accordion-item">
+            <h2 className="accordion-header">
+            <button className="accordion-button collapsed" style={myStyle} type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+              <strong>  Browser Compatible</strong>
+            </button>
+            </h2>
+            <div id="collapseThree" className="accordion-collapse collapse"  style={myStyle} data-bs-parent="#accordionExample">
+            <div className="accordion-body">
+                <strong>This app is fully compatible with all modern web browsers including Google Chrome, Mozilla Firefox, Microsoft Edge, and Safari. Whether you're on a laptop, desktop, tablet, or mobile device, TextUtils ensures a smooth, responsive experience without requiring additional plugins or downloads. It works right out of the box.</strong>
+            </div>
+            </div>
+        </div>
+      
+</div>
+    </div>
+  )
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import {Link} from 'react-router-dom';
+
+export default function Navbar(props) {
+  return (
+    <>
+      <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
+  <div className="container-fluid">
+    <Link className="navbar-brand" to="/">{props.title}</Link>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <Link className="nav-a " aria-current="page" to="/">Home</Link>
+        </li>
+        <li className="nav-item mx-3">
+          <Link    className="nav-a" to="/about">{props.AboutText}</Link>
+        </li>
+        
+      </ul>
+      <form className="d-flex" role="search">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-primary" type="submit">Search</button>
+      </form>
+      <div className={`form-check form-switch text-${props.mode==='light'? 'dark' : 'light'}`}>
+      <label >Select Theme:</label>
+      <button className="btn btn-sm btn-outline-dark mx-1" onClick={()=>props.toggleMode('dark')} style={{ color: '#17cbd1', border: '2px solid #17cbd1', backgroundColor: 'transparent' }}>Dark</button>
+      <button className="btn btn-sm btn-outline-dark mx-1" onClick={()=>props.toggleMode('green')} style={{ color: 'green', border: '2px solid green', backgroundColor: 'transparent' } } >Green</button>
+      <button className="btn btn-sm btn-outline-dark mx-1" onClick={()=>props.toggleMode('red')}  style={{ color: 'red', border: '2px solid red', backgroundColor: 'transparent' }}>Red</button>
+      <button className="btn btn-sm btn-outline-dark mx-1" onClick={()=>props.toggleMode('light')}style={{ color: 'gray', border: '2px solid gray', backgroundColor: 'transparent' }} >Light</button>
+      
+     
+    </div>
+    </div>
+  </div>
+</nav>
+    </>
+  )
+}
